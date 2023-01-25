7af4595e6cde0f2f8a8e304c9c70a59a330c8a8a
Vitali Avagyan <eheva87@gmail.com>
2023-01-25 03:58:00 +0000

    error: compuational graph or cuda

--- session15_rnn_lstm.py
+++ session15_rnn_lstm.py
@@ -1,4 +1,4 @@
-# Using RNN sentiment analysis
+# Using RNN for sentiment analysis
 
 import numpy as np
 import torch
@@ -8,34 +8,38 @@ from collections import Counter, OrderedDict
 from torchtext.vocab import vocab
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.preprocessing import OneHotEncoder
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
 
+torch.autograd.set_detect_anomaly(True)
+
+# DATA
 data = pd.read_csv('AmazonReview.csv')
 data.head()
 data.dropna(inplace=True)
 data.reset_index(inplace=True, drop=True)
 
+# ENCODING
 # 1,2,3->negative(i.e 0)
 data.loc[data['Sentiment'] <= 3, 'Sentiment'] = 0
-
 # 4,5->positive(i.e 1)
 data.loc[data['Sentiment'] > 3, 'Sentiment'] = 1
 
+# SPLIT
 train_idx, test_idx, y_train, y_test = train_test_split(
     data.index, data["Sentiment"], test_size=0.2, stratify=data["Sentiment"])
 
-y_train_np = np.array(y_train)
-y_test_np = np.array(y_test)
+y_train_np = np.array(y_train, dtype=np.double)
+y_test_np = np.array(y_test, dtype=np.double)
 
 X_train = data.iloc[train_idx, 0]
 X_test = data.iloc[test_idx, 0]
 
-train_dataset = TensorDataset(torch.from_numpy(train_idx.to_numpy()), torch.from_numpy(y_train_np))
-test_dataset = TensorDataset(torch.from_numpy(test_idx.to_numpy()), torch.from_numpy(y_test_np))
+train_dataset = TensorDataset(torch.from_numpy(train_idx.to_numpy()).double(), torch.from_numpy(y_train_np))
+test_dataset = TensorDataset(torch.from_numpy(test_idx.to_numpy()).double(), torch.from_numpy(y_test_np))
 
 MAX_VOCAB_SIZE = 1_000
+TIMESTEPS = 500
 BATCH_SIZE = 16
 
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
@@ -59,57 +63,60 @@ encoder.fit(vals.reshape(-1, 1))
 
 FULL_VOCAB_SIZE = len(v1)
 
+
 class RNN(nn.Module):
     def __init__(self, data_size, hidden_size, output_size):
         super().__init__()
 
         self.hidden_size = hidden_size
-        input_size = data_size + hidden_size
+        self.output_size = output_size
+        self.data_size = data_size
 
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, output_size)
+        input_size = self.data_size + self.hidden_size
+
+        self.i2h = nn.Linear(input_size, self.hidden_size, dtype=torch.double)
+        self.h2o = nn.Linear(self.hidden_size, self.output_size, dtype=torch.double)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, data, last_hidden):
-        input_ = torch.cat((data, last_hidden), 1)
+    def forward(self, data_, last_hidden):
+        input_ = torch.cat((data_, last_hidden), 1).double()
         hidden_ = self.i2h(input_)
-        output_ = self.sigmoid(self.h2o(hidden_))
+        output_1 = self.h2o(hidden_)
+        output_ = self.sigmoid(output_1.clone())
         return hidden_, output_
 
-loss_fn = nn.MSELoss()
-rnn = RNN(FULL_VOCAB_SIZE, FULL_VOCAB_SIZE, 1).to("cuda")
+
+loss_fn = nn.BCEWithLogitsLoss()
+rnn = RNN(FULL_VOCAB_SIZE, FULL_VOCAB_SIZE, 1).to('cuda')
 optimiser = torch.optim.SGD(rnn.parameters(), lr=0.001)
 
-hidden = torch.zeros(BATCH_SIZE, FULL_VOCAB_SIZE).to("cuda")
-target = torch.zeros(BATCH_SIZE, 1).to("cuda")
+hidden = torch.zeros(BATCH_SIZE, FULL_VOCAB_SIZE, dtype=torch.double).to('cuda')
+target = torch.zeros(BATCH_SIZE, 1, dtype=torch.double).to('cuda')
 
-# max_len = 500
+PADS = ("<pad> " * TIMESTEPS)[:-1]
+G = torch.ones(BATCH_SIZE, TIMESTEPS, FULL_VOCAB_SIZE).to('cuda')
 
 for train_indices, y_train in train_dataloader:
+    y_train = y_train.to("cuda")
 
     X_train = data.iloc[train_indices, 0]
 
-    V = [v1.lookup_indices(x.split()) for x in X_train]
+    V = [v1.lookup_indices(x[:TIMESTEPS].split()) for x in X_train]
+    V.append(v1.lookup_indices(PADS.split()))
 
     padded_sentences = pad_sequence([torch.tensor(p) for p in V], batch_first=True, padding_value=1)
 
-    # for i in range(padded_sentences.shape[0]):
-    #     padded_sentences[i] = nn.ConstantPad1d((0, max_len - padded_sentences[i].shape[0]), 1)(padded_sentences[i])
-
-    TIMESTEPS = len(padded_sentences[0])
-
-    G = torch.ones(BATCH_SIZE, TIMESTEPS, FULL_VOCAB_SIZE).to("cuda")
     for i in range(BATCH_SIZE):
         for j in range(TIMESTEPS):
             G[i, j] = torch.tensor(encoder.transform(padded_sentences[i, j].reshape(-1, 1))[0])
 
-    for i in range(TIMESTEPS):
-        hidden, output = rnn(G[:,i], hidden)
-        if i == TIMESTEPS - 1:
-            loss = loss_fn(output, target)
-            print(f"TimeStep {i}, loss: {loss}")
+    for k in range(TIMESTEPS):
+        hidden, output = rnn(G[:, k], hidden)
+        if k == TIMESTEPS - 1:
+            loss = loss_fn(output, y_train.reshape(-1, 1))
+            # loss.to("cuda")
+            print(f"TimeStep {k}, loss: {loss}")
             optimiser.zero_grad()
             loss.backward(retain_graph=True)
-            optimiser.zero_grad()
             optimiser.step()
             print(optimiser.param_groups)
